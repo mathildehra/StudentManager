b80cdd48834a2ec5ea1c6b65d804e8f9bdbba385
mathildehra <mathilde.harrau@gmail.com>
2026-03-31 15:25:42 +0300

    c'est good

---
+++ TeacherDashboardController.java
@@ -0,0 +1,22 @@
+package me.serbob.demo;
+
+import javafx.fxml.FXML;
+
+public class TeacherDashboardController {
+    @FXML
+    protected void onViewStudentsClick() {
+        // Load view students screen
+    }
+    
+    @FXML
+    protected void onAddStudentsClick() {
+        // Load add students screen
+        // Handle "notification already exists" constraint
+    }
+    
+    @FXML
+    protected void onAddGradesClick() {
+        // Load add grades screen
+        // This should lead to "graphs charts" view
+    }
+}
